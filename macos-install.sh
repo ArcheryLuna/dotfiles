e3a42458e1fe05dda33eed8f3069d488fb239457
ArcheryLuna <76516425+ArcheryLuna@users.noreply.github.com>
2024-02-10 21:42:14 -0800

    Install script

--- macos-install.sh
+++ macos-install.sh
@@ -1,7 +1,3 @@
-xcode-select --install
-
-git clone https://github.com/ArcheryLuna/dotfiles.git ~/.dotfiles
-
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew bundle --file ~/.dotfiles/macos/Brewfile
 
